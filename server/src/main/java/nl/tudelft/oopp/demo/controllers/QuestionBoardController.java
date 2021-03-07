b68f273d7152c40db55c0c2b863549308607276c
Gijs van de Linde <G.vandeLinde@student.tudelft.nl>
2021-03-07 14:42:34 +0100

    Switch controller to the use of a service for the sake of Dependency Injection

--- server/src/main/java/nl/tudelft/oopp/demo/controllers/QuestionBoardController.java
+++ server/src/main/java/nl/tudelft/oopp/demo/controllers/QuestionBoardController.java
@@ -5,25 +5,18 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import java.util.Set;
 import java.util.UUID;
 import nl.tudelft.oopp.demo.entities.Question;
-import nl.tudelft.oopp.demo.entities.QuestionBoard;
-import nl.tudelft.oopp.demo.repositories.QuestionBoardRepository;
-import nl.tudelft.oopp.demo.repositories.QuestionRepository;
+import nl.tudelft.oopp.demo.services.QuestionBoardService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class QuestionBoardController {
 
     @Autowired
-    private QuestionBoardRepository repository;
-
-    @Autowired
-    private QuestionRepository questionRepository;
+    QuestionBoardService questionBoardService;
 
     /**
      * GET endpoint to retrieve list of questions in that board.
@@ -34,13 +27,6 @@ public class QuestionBoardController {
     @RequestMapping(value = "/api/board/{boardid}/questions", method = GET)
     @ResponseBody
     public Set<Question> retrieveQuestionListByBoardId(@PathVariable("boardid") UUID boardId) {
-        // 400 is thrown upon bad formatting automatically
-        QuestionBoard qb = repository.getById(boardId);
-        if (qb == null) {
-            // Throw 404 when board does not exist
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find resource");
-        }
-        // Find questions belonging to this board
-        return questionRepository.getQuestionByQuestionBoard(qb);
+        return questionBoardService.retrieveQuestions(boardId);
     }
 }
