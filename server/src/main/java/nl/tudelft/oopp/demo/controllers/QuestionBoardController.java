23d78605e3fc8c01af5169dc9eb8bd4a0118be90
Gijs van de Linde <G.vandeLinde@student.tudelft.nl>
2021-03-08 14:36:29 +0100

    Add and switch to use of binding model for creating a new QuestionBoard

--- server/src/main/java/nl/tudelft/oopp/demo/controllers/QuestionBoardController.java
+++ server/src/main/java/nl/tudelft/oopp/demo/controllers/QuestionBoardController.java
@@ -4,10 +4,12 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 import java.sql.Timestamp;
 
+import nl.tudelft.oopp.demo.bindingmodels.QuestionBoardBindingModel;
 import nl.tudelft.oopp.demo.entities.QuestionBoard;
 import nl.tudelft.oopp.demo.services.QuestionBoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,11 +20,9 @@ public class QuestionBoardController {
     @Autowired
     QuestionBoardService service;
 
-    @RequestMapping(value = "/api/board", method = POST)
+    @RequestMapping(value = "/api/board", method = POST, consumes = "application/json")
     @ResponseBody
-    public QuestionBoard createBoard(@RequestParam("title") String title,
-                                     @RequestParam("startdate") Timestamp startDate,
-                                     @RequestParam("enddate") Timestamp endDate) {
-        return service.saveBoard(new QuestionBoard(title, startDate, endDate));
+    public QuestionBoard createBoard(@RequestBody QuestionBoardBindingModel qb) {
+        return service.saveBoard(qb);
     }
 }
--- server/src/main/java/nl/tudelft/oopp/demo/services/QuestionBoardService.java
+++ server/src/main/java/nl/tudelft/oopp/demo/services/QuestionBoardService.java
@@ -1,5 +1,6 @@
 package nl.tudelft.oopp.demo.services;
 
+import nl.tudelft.oopp.demo.bindingmodels.QuestionBoardBindingModel;
 import nl.tudelft.oopp.demo.entities.QuestionBoard;
 import nl.tudelft.oopp.demo.repositories.QuestionBoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +12,14 @@ public class QuestionBoardService {
     @Autowired
     private QuestionBoardRepository questionBoardRepository;
 
-    public QuestionBoard saveBoard(QuestionBoard board) {
+    /**
+     * Reflect binding model of question board and save it to the DB.
+     *
+     * @param boardModel    The board model.
+     * @return The QuestionBoard that was created.
+     */
+    public QuestionBoard saveBoard(QuestionBoardBindingModel boardModel) {
+        QuestionBoard board = boardModel.reflect();
         questionBoardRepository.save(board);
         return board;
     }
