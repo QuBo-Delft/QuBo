3fbd8ea6c5a42cd7a83ad2c876429b8c071541d7
Gijs van de Linde <G.vandeLinde@student.tudelft.nl>
2021-03-10 20:27:20 +0100

    Solve merge conflicts

--- server/src/main/java/nl/tudelft/oopp/demo/controllers/QuestionBoardController.java
+++ server/src/main/java/nl/tudelft/oopp/demo/controllers/QuestionBoardController.java
@@ -1,31 +1,24 @@
 package nl.tudelft.oopp.demo.controllers;
 
-
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 import java.util.Set;
 import java.util.UUID;
+
+import nl.tudelft.oopp.demo.bindingmodels.QuestionBoardBindingModel;
 import nl.tudelft.oopp.demo.entities.Question;
+import nl.tudelft.oopp.demo.entities.QuestionBoard;
 import nl.tudelft.oopp.demo.services.QuestionBoardService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.server.ResponseStatusException;
-
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
-
-import java.sql.Timestamp;
-
-import nl.tudelft.oopp.demo.bindingmodels.QuestionBoardBindingModel;
-import nl.tudelft.oopp.demo.entities.QuestionBoard;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 
 @Controller
 public class QuestionBoardController {
@@ -36,7 +29,7 @@ public class QuestionBoardController {
     /**
      * GET endpoint to retrieve the list of questions of this QuestionBoard.
      *
-     * @param boardId   ID property of a board.
+     * @param boardId ID property of a board.
      * @return The list of questions of this board.
      */
     @RequestMapping(value = "/api/board/{boardid}/questions", method = GET)
@@ -49,7 +42,7 @@ public class QuestionBoardController {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find resource");
         }
         return ql;
-
+    }
 
     @RequestMapping(value = "/api/board", method = POST, consumes = "application/json")
     @ResponseBody
--- server/src/main/java/nl/tudelft/oopp/demo/services/QuestionBoardService.java
+++ server/src/main/java/nl/tudelft/oopp/demo/services/QuestionBoardService.java
@@ -2,19 +2,15 @@ package nl.tudelft.oopp.demo.services;
 
 import java.util.Set;
 import java.util.UUID;
+
+import nl.tudelft.oopp.demo.bindingmodels.QuestionBoardBindingModel;
 import nl.tudelft.oopp.demo.entities.Question;
 import nl.tudelft.oopp.demo.entities.QuestionBoard;
 import nl.tudelft.oopp.demo.repositories.QuestionBoardRepository;
 import nl.tudelft.oopp.demo.repositories.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
-import nl.tudelft.oopp.demo.bindingmodels.QuestionBoardBindingModel;
-import nl.tudelft.oopp.demo.entities.QuestionBoard;
-import nl.tudelft.oopp.demo.repositories.QuestionBoardRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+
 
 @Service
 public class QuestionBoardService {
@@ -31,17 +27,18 @@ public class QuestionBoardService {
      * @return The set of questions of this specific board.
      */
     public Set<Question> getQuestionsByBoardId(UUID boardId) {
-        QuestionBoard qb = repository.getById(boardId);
+        QuestionBoard qb = questionBoardRepository.getById(boardId);
         if (qb == null) {
             return null;
         }
         // Find questions belonging to this board
         return questionRepository.getQuestionByQuestionBoard(qb);
+    }
 
     /**
      * Reflect binding model of question board and save it to the DB.
      *
-     * @param boardModel    The board model.
+     * @param boardModel The board model.
      * @return The QuestionBoard that was created.
      */
     public QuestionBoard saveBoard(QuestionBoardBindingModel boardModel) {
