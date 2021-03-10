ca6410189bc651a951a5699fd816d45033f5b38a
Yunhan Wang <y.wang-128@student.tudelft.nl>
2021-03-10 22:15:00 +0000

    Improve code style

--- client/src/main/java/nl/tudelft/oopp/demo/views/ConfirmationDialog.java
+++ client/src/main/java/nl/tudelft/oopp/demo/views/ConfirmationDialog.java
@@ -12,42 +12,35 @@ public class ConfirmationDialog {
     static boolean answer;
 
     /**
-     * This method gets user's choice after clicking yes or no button
+     * This method gets user's choice after clicking yes or no button.
      *
-     * @param title the title of window
-     * @param message the message to be displayed
-     * @return true iff the user clicked "yes"
+     * @param title the title of window.
+     * @param message the message to be displayed.
+     * @return true iff the user clicked "yes".
      */
     public static boolean display(String title, String message) {
         Stage window = new Stage();
         window.initModality(Modality.APPLICATION_MODAL);
         window.setTitle(title);
         window.setMinWidth(250);
-
         Label label = new Label();
         label.setText(message);
-
-        Button yesButton = new Button("yes");
-        Button noButton = new Button("no");
-
+        Button yesButton = new Button("Yes");
+        Button noButton = new Button("No");
         yesButton.setOnAction(e -> {
             answer = true;
             window.close();
         });
-
         noButton.setOnAction(e -> {
             answer = false;
             window.close();
         });
-
         VBox layout = new VBox(10);
         layout.getChildren().addAll(label, yesButton, noButton);
         layout.setAlignment(Pos.CENTER);
-
         Scene scene = new Scene(layout);
         window.setScene(scene);
         window.showAndWait();
-
         return answer;
     }
 }
--- client/src/main/java/nl/tudelft/oopp/demo/views/GetTextDialog.java
+++ client/src/main/java/nl/tudelft/oopp/demo/views/GetTextDialog.java
@@ -14,39 +14,32 @@ public class GetTextDialog {
     static String userInput;
 
     /**
-     * This method gets user's text input after clicking submission button
+     * This method gets user's text input after clicking submission button.
      *
-     * @param title the title of dialog
-     * @param message  the message to be displayed
-     * @return the user text input
+     * @param title the title of dialog.
+     * @param message  the message to be displayed.
+     * @return the user text input.
      */
     public static String display(String title, String message) {
         Stage window = new Stage();
         window.initModality(Modality.APPLICATION_MODAL);
         window.setTitle(title);
         window.setMinWidth(250);
-
         Label label = new Label();
         label.setText(message);
-
         TextArea inputField = new TextArea();
         inputField.setPromptText("Please enter the text");
-
         Button submitButton = new Button("Confirm submission");
-
         submitButton.setOnAction(e -> {
             userInput = inputField.getText();
             window.close();
         });
-
         VBox layout = new VBox(15);
         layout.getChildren().addAll(label, inputField, submitButton);
         layout.setAlignment(Pos.CENTER);
-
         Scene scene = new Scene(layout);
         window.setScene(scene);
         window.showAndWait();
-
         return userInput;
     }
 }
