3ea85ef221e6549235ddd5cc6b556e154d33e4e0
Yunhan Wang <y.wang-128@student.tudelft.nl>
2021-03-12 16:48:38 +0000

    Improve javadoc and make the code more readable

--- client/src/main/java/nl/tudelft/oopp/demo/views/ConfirmationDialog.java
+++ client/src/main/java/nl/tudelft/oopp/demo/views/ConfirmationDialog.java
@@ -9,26 +9,32 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class ConfirmationDialog {
+
     static boolean answer;
 
     /**
-     * This method gets user's choice after clicking Tes or No button.
+     * This method creates a dialog window with a question as the message of the window. 
+     * Users can click on the "Yes" and "No" buttons in this dialog window to answer said question.
      *
      * @param title     The title of this dialog.
      * @param message   The message to be displayed.
-     * @return True iff the user clicked "yes".
+     * @return True if and only if the user clicked "Yes".
      */
     public static boolean display(String title, String message) {
+
         Stage window = new Stage();
         // Block user action
         window.initModality(Modality.APPLICATION_MODAL);
+
         window.setTitle(title);
         window.setMinWidth(250);
-        // Create components
+
         Label label = new Label();
+        // Set label to the message
         label.setText(message);
         Button yesButton = new Button("Yes");
         Button noButton = new Button("No");
+
         // Set button click action
         yesButton.setOnAction(e -> {
             answer = true;
@@ -38,15 +44,18 @@ public class ConfirmationDialog {
             answer = false;
             window.close();
         });
+
         VBox layout = new VBox(10);
         // Add components to layout
         layout.getChildren().addAll(label, yesButton, noButton);
         layout.setAlignment(Pos.CENTER);
+        
         Scene scene = new Scene(layout);
         // Display this dialog
         window.setScene(scene);
-        // Can return back only if close the current dialog
+        // Can return back only if the current dialog is closed
         window.showAndWait();
+
         // Return user's choice: true or false
         return answer;
     }
--- client/src/main/java/nl/tudelft/oopp/demo/views/GetTextDialog.java
+++ client/src/main/java/nl/tudelft/oopp/demo/views/GetTextDialog.java
@@ -14,38 +14,48 @@ public class GetTextDialog {
     static String userInput;
 
     /**
-     * This method gets user's text input after clicking submission button.
+     * This method creates a dialog window which allows users to enter text. 
+     * After the user presses "Confirm submission" the textual input is returned 
+     * and the dialog window disappears.
      *
      * @param title     The title of this dialog.
      * @param message   The message to be displayed.
      * @return The user's text input.
      */
     public static String display(String title, String message) {
+
         Stage window = new Stage();
+
         // Block user action
         window.initModality(Modality.APPLICATION_MODAL);
+
         window.setTitle(title);
         window.setMinWidth(250);
-        // Create components
+
         Label label = new Label();
         label.setText(message);
+
         TextArea inputField = new TextArea();
         inputField.setPromptText("Please enter the text");
+
         Button submitButton = new Button("Confirm submission");
         // Set button click action
         submitButton.setOnAction(e -> {
             userInput = inputField.getText();
             window.close();
         });
+
         VBox layout = new VBox(15);
         layout.getChildren().addAll(label, inputField, submitButton);
         // Center all components
         layout.setAlignment(Pos.CENTER);
+
         Scene scene = new Scene(layout);
         // Display the dialog
         window.setScene(scene);
-        // Can return back only if close the current dialog
+        // Can return back only if the current dialog is closed
         window.showAndWait();
+        
         // Return user's text input
         return userInput;
     }
