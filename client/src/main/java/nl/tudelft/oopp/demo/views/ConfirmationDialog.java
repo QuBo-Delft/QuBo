755bee3bcad01a0765837cecfacd06af3c0d9969
Yunhan Wang <y.wang-128@student.tudelft.nl>
2021-03-12 16:21:40 +0000

    Add annotations to the code

--- client/src/main/java/nl/tudelft/oopp/demo/views/ConfirmationDialog.java
+++ client/src/main/java/nl/tudelft/oopp/demo/views/ConfirmationDialog.java
@@ -14,19 +14,22 @@ public class ConfirmationDialog {
     /**
      * This method gets user's choice after clicking Tes or No button.
      *
-     * @param title     The title of window.
+     * @param title     The title of this dialog.
      * @param message   The message to be displayed.
      * @return True iff the user clicked "yes".
      */
     public static boolean display(String title, String message) {
         Stage window = new Stage();
+        // Block user action
         window.initModality(Modality.APPLICATION_MODAL);
         window.setTitle(title);
         window.setMinWidth(250);
+        // Create components
         Label label = new Label();
         label.setText(message);
         Button yesButton = new Button("Yes");
         Button noButton = new Button("No");
+        // Set button click action
         yesButton.setOnAction(e -> {
             answer = true;
             window.close();
@@ -36,11 +39,15 @@ public class ConfirmationDialog {
             window.close();
         });
         VBox layout = new VBox(10);
+        // Add components to layout
         layout.getChildren().addAll(label, yesButton, noButton);
         layout.setAlignment(Pos.CENTER);
         Scene scene = new Scene(layout);
+        // Display this dialog
         window.setScene(scene);
+        // Can return back only if close the current dialog
         window.showAndWait();
+        // Return user's choice: true or false
         return answer;
     }
 }
