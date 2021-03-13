caa199b72b25fdc65f46a27198084eeae793d61a
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-03-14 00:22:01 +0200

    Create QuestionBoardDetailsDto

---
+++ common/src/main/java/nl/tudelft/oopp/demo/dtos/QuestionBoardDetailsDto.java
@@ -0,0 +1,49 @@
+package nl.tudelft.oopp.demo.dtos;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+public class QuestionBoardDetailsDto {
+    private UUID id;
+
+    private String title;
+
+    private Timestamp startTime;
+
+    private Timestamp endTime;
+
+    public QuestionBoardDetailsDto() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Timestamp getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Timestamp startTime) {
+        this.startTime = startTime;
+    }
+
+    public Timestamp getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Timestamp endTime) {
+        this.endTime = endTime;
+    }
+}
