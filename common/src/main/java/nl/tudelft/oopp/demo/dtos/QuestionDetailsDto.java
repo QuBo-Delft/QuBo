54b2619dcfeb52537bf385dc249ba5c34d979941
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-03-14 00:44:21 +0200

    Create QuestionDetailsDto

---
+++ common/src/main/java/nl/tudelft/oopp/demo/dtos/QuestionDetailsDto.java
@@ -0,0 +1,50 @@
+package nl.tudelft.oopp.demo.dtos;
+
+import java.sql.Timestamp;
+import java.util.Set;
+import java.util.UUID;
+
+public class QuestionDetailsDto {
+    private UUID id;
+
+    private String text;
+
+    private Timestamp timestamp;
+
+    private Set<AnswerDetailsDto> answers;
+
+    public QuestionDetailsDto() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Set<AnswerDetailsDto> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(Set<AnswerDetailsDto> answers) {
+        this.answers = answers;
+    }
+}
