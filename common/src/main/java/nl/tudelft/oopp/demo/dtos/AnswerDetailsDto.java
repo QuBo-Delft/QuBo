456fdf381821f00f43016d938ae73a06452a4152
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-03-14 00:44:29 +0200

    Create AnswerDetailsDto

---
+++ common/src/main/java/nl/tudelft/oopp/demo/dtos/AnswerDetailsDto.java
@@ -0,0 +1,39 @@
+package nl.tudelft.oopp.demo.dtos;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+public class AnswerDetailsDto {
+    private UUID id;
+
+    private String text;
+
+    private Timestamp timestamp;
+
+    public AnswerDetailsDto() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
