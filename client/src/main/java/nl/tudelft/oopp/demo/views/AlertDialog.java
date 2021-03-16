b89f379f1061222e02db35927f170640b4c41f90
Yunhan Wang <Y.Wang-128@student.tudelft.nl>
2021-03-16 20:21:04 +0100

    Improve javadoc for alert dialog

--- client/src/main/java/nl/tudelft/oopp/demo/views/AlertDialog.java
+++ client/src/main/java/nl/tudelft/oopp/demo/views/AlertDialog.java
@@ -4,10 +4,10 @@ import javafx.scene.control.Alert;
 
 public class AlertDialog {
     /**
-     * This is an alert dialog that will pop up when called with alert title and message
+     * This is an alert dialog that will pop up when called with alert title and message.
      *
-     * @param title     The title of this alert
-     * @param message   The message to be displayed
+     * @param title     The title of this alert.
+     * @param message   The message to be displayed.
      */
     public static void display(String title, String message) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
