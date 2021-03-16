31323afda52f579cd2a93aa45f49ee1d884c53df
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-03-16 13:52:23 +0200

    Add getByModeratorCode method in QuestionBoardRepository

--- server/src/main/java/nl/tudelft/oopp/demo/repositories/QuestionBoardRepository.java
+++ server/src/main/java/nl/tudelft/oopp/demo/repositories/QuestionBoardRepository.java
@@ -2,7 +2,6 @@ package nl.tudelft.oopp.demo.repositories;
 
 import java.util.List;
 import java.util.UUID;
-import nl.tudelft.oopp.demo.entities.Question;
 import nl.tudelft.oopp.demo.entities.QuestionBoard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,6 +9,8 @@ import org.springframework.stereotype.Repository;
 @Repository("QuestionBoardRepository")
 public interface QuestionBoardRepository extends JpaRepository<QuestionBoard, UUID> {
     QuestionBoard getById(UUID id);
+
+    QuestionBoard getByModeratorCode(UUID code);
 
     List<QuestionBoard> findAll();
 
