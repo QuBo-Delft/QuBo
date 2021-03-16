da319c31f9e28480dc79b4a52f316a640cda1689
Yunhan Wang <Y.Wang-128@student.tudelft.nl>
2021-03-16 20:17:33 +0100

    Add a general purpose alert dialog

---
+++ client/src/main/java/nl/tudelft/oopp/demo/views/AlertDialog.java
@@ -0,0 +1,20 @@
+package nl.tudelft.oopp.demo.views;
+
+import javafx.scene.control.Alert;
+
+public class AlertDialog {
+    /**
+     * This is an alert dialog that will pop up when called with alert title and message
+     *
+     * @param title     The title of this alert
+     * @param message   The message to be displayed
+     */
+    public static void display(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+}
