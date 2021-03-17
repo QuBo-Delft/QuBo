e4b9078a901ed348b4ed92e6cbf7729df6e16726
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-03-17 18:50:22 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- server/src/main/java/nl/tudelft/oopp/demo/controllers/QuestionBoardController.java
+++ server/src/main/java/nl/tudelft/oopp/demo/controllers/QuestionBoardController.java
@@ -80,7 +80,7 @@ public class QuestionBoardController {
     }
 
     /**
-     * GET endpoint that provides client with the QuestionBoard with the specified moderator code.
+     * GET endpoint that provides the client with the QuestionBoard associated with the specified moderator code.
      * Throw 400 upon wrong UUID formatting.
      * Throw 404 upon requesting a non-existent moderator code.
      *
