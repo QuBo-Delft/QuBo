9d2c318d950e3c5558c221362be94cf8783f500c
Gijs van de Linde <G.vandeLinde@student.tudelft.nl>
2021-03-18 17:20:12 +0100

    Create binding model and DTO for PaceVote entities

---
+++ common/src/main/java/nl/tudelft/oopp/demo/dtos/pacevote/PaceVoteCreationDto.java
@@ -0,0 +1,18 @@
+package nl.tudelft.oopp.demo.dtos.pacevote;
+
+import java.util.UUID;
+
+public class PaceVoteCreationDto {
+    private UUID id;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public PaceVoteCreationDto() {
+    }
+}
