b770ad8a76eb0712e3b78792a69f3a4fc435a96e
Gijs van de Linde <G.vandeLinde@student.tudelft.nl>
2021-03-18 17:55:26 +0100

    Change AnswerCreationDto to contain only the AnswerId and update tests

--- server/src/test/java/nl/tudelft/oopp/demo/mappings/answer/AnswerCreationBindingModelTests.java
+++ server/src/test/java/nl/tudelft/oopp/demo/mappings/answer/AnswerCreationBindingModelTests.java
@@ -1,16 +1,10 @@
 package nl.tudelft.oopp.demo.mappings.answer;
 
 import nl.tudelft.oopp.demo.dtos.answer.AnswerCreationBindingModel;
-import nl.tudelft.oopp.demo.dtos.answer.AnswerDetailsDto;
-import nl.tudelft.oopp.demo.dtos.question.QuestionCreationBindingModel;
 import nl.tudelft.oopp.demo.entities.Answer;
-import nl.tudelft.oopp.demo.entities.Question;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.modelmapper.ModelMapper;
-
-import java.sql.Timestamp;
-import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
--- server/src/test/java/nl/tudelft/oopp/demo/mappings/answer/AnswerCreationDtoTests.java
+++ server/src/test/java/nl/tudelft/oopp/demo/mappings/answer/AnswerCreationDtoTests.java
@@ -4,13 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.sql.Timestamp;
 import java.util.UUID;
-import nl.tudelft.oopp.demo.dtos.answer.AnswerDetailsDto;
+import nl.tudelft.oopp.demo.dtos.answer.AnswerCreationDto;
 import nl.tudelft.oopp.demo.entities.Answer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.modelmapper.ModelMapper;
 
-public class AnswerDetailsDtoTests {
+public class AnswerCreationDtoTests {
     private ModelMapper mapper;
 
     @BeforeEach
@@ -23,15 +23,11 @@ public class AnswerDetailsDtoTests {
         // Arrange
         Answer answer = new Answer();
         answer.setId(UUID.fromString("93c8f1d7-7265-4596-9320-ae34895ff64a"));
-        answer.setText("Answer Text");
-        answer.setTimestamp(Timestamp.valueOf("2021-03-01 10:05:00"));
 
         // Act
-        AnswerDetailsDto dto = mapper.map(answer, AnswerDetailsDto.class);
+        AnswerCreationDto dto = mapper.map(answer, AnswerCreationDto.class);
 
         // Assert
         assertEquals(answer.getId(), dto.getId());
-        assertEquals(answer.getText(), dto.getText());
-        assertEquals(answer.getTimestamp(), dto.getTimestamp());
     }
 }
