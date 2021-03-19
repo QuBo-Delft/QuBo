ced613313fa04f31b94d71b085d8a76f177f20c2
Gijs van de Linde <G.vandeLinde@student.tudelft.nl>
2021-03-19 11:48:38 +0100

    Fix accidental changes made to the tests

--- server/src/test/java/nl/tudelft/oopp/demo/mappings/answer/AnswerDetailsDtoTests.java
+++ server/src/test/java/nl/tudelft/oopp/demo/mappings/answer/AnswerDetailsDtoTests.java
@@ -1,16 +1,19 @@
 package nl.tudelft.oopp.demo.mappings.answer;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.sql.Timestamp;
-import java.util.UUID;
 import nl.tudelft.oopp.demo.dtos.answer.AnswerCreationDto;
+import nl.tudelft.oopp.demo.dtos.answer.AnswerDetailsDto;
 import nl.tudelft.oopp.demo.entities.Answer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.modelmapper.ModelMapper;
 
-public class AnswerCreationDtoTests {
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AnswerDetailsDtoTests {
     private ModelMapper mapper;
 
     @BeforeEach
@@ -19,15 +22,19 @@ public class AnswerCreationDtoTests {
     }
 
     @Test
-    public void mapFromAnswerToAnswerCreationDto() {
+    public void mapFromAnswerToAnswerDetailsDto() {
         // Arrange
         Answer answer = new Answer();
         answer.setId(UUID.fromString("93c8f1d7-7265-4596-9320-ae34895ff64a"));
+        answer.setTimestamp(Timestamp.from(Instant.now()));
+        answer.setText("Test");
 
         // Act
-        AnswerCreationDto dto = mapper.map(answer, AnswerCreationDto.class);
+        AnswerDetailsDto dto = mapper.map(answer, AnswerDetailsDto.class);
 
         // Assert
         assertEquals(answer.getId(), dto.getId());
+        assertEquals(answer.getTimestamp(), dto.getTimestamp());
+        assertEquals(answer.getText(), dto.getText());
     }
 }
