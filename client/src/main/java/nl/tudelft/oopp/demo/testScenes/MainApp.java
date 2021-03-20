2d9036958f55e0602b8a209dbf44a112fbcff0b3
Yunhan Wang <Y.Wang-128@student.tudelft.nl>
2021-03-20 23:51:44 +0100

    Add the mainApp class for testing

---
+++ client/src/main/java/nl/tudelft/oopp/demo/testScenes/MainApp.java
@@ -0,0 +1,21 @@
+package nl.tudelft.oopp.demo.testScenes;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import nl.tudelft.oopp.demo.communication.ServerCommunication;
+import nl.tudelft.oopp.demo.dtos.questionboard.QuestionBoardCreationBindingModel;
+import nl.tudelft.oopp.demo.dtos.questionboard.QuestionBoardCreationDto;
+import nl.tudelft.oopp.demo.views.QuoteDisplay;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        SceneDisplay.main(new String[0]);
+
+    }
+
+}
