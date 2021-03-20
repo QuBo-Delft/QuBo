fa769207f85b6f2b17ba11c414c5e07a729bf634
Yunhan Wang <y.wang-128@student.tudelft.nl>
2021-03-20 22:27:02 +0000

    Change the fxml file to be loaded to mainScene in QuoteDisplay

--- client/src/main/java/nl/tudelft/oopp/demo/views/QuoteDisplay.java
+++ client/src/main/java/nl/tudelft/oopp/demo/views/QuoteDisplay.java
@@ -14,7 +14,7 @@ public class QuoteDisplay extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         FXMLLoader loader = new FXMLLoader();
-        URL xmlUrl = getClass().getResource("/CreateQuestionBoardWindow.fxml");
+        URL xmlUrl = getClass().getResource("/mainScene.fxml");
         loader.setLocation(xmlUrl);
         Parent root = loader.load();
 
