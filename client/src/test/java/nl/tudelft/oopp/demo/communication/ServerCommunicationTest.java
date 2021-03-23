8b9d07ae3cba12733c454a50a4dab2cfe445be89
Cassandra Visser <C.M.S.Visser@student.tudelft.nl>
2021-03-23 22:57:43 +0100

    Add tests for retrieveQuestions Adds one test that tests the method with a valid board ID without any questions, one that tests it with a valid board ID with questions, and one that tests it with an invalid code.

--- client/src/test/java/nl/tudelft/oopp/demo/communication/ServerCommunicationTest.java
+++ client/src/test/java/nl/tudelft/oopp/demo/communication/ServerCommunicationTest.java
@@ -2,6 +2,8 @@ package nl.tudelft.oopp.demo.communication;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import nl.tudelft.oopp.demo.dtos.question.QuestionCreationDto;
+import nl.tudelft.oopp.demo.dtos.question.QuestionDetailsDto;
 import nl.tudelft.oopp.demo.dtos.questionboard.QuestionBoardCreationBindingModel;
 import nl.tudelft.oopp.demo.dtos.questionboard.QuestionBoardCreationDto;
 import nl.tudelft.oopp.demo.dtos.questionboard.QuestionBoardDetailsDto;
@@ -10,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
 import java.util.UUID;
 
 //To run these tests, one must first run DemoApplication.java in the server folder as these tests require
@@ -189,6 +192,74 @@ public class ServerCommunicationTest {
 
         //Assert
         assertNull(quBoDetails);
+    }
+
+    //Tests if retrieveQuestions returns an empty array when called through a valid board ID without
+    //any questions.
+    @Test
+    public void testRetrieveQuestionsEmptyArray() {
+        //Arrange
+        QuestionBoardCreationBindingModel quBoModel = new QuestionBoardCreationBindingModel();
+        quBoModel.setStartTime(Timestamp.from(Instant.now().truncatedTo(ChronoUnit.SECONDS)));
+        quBoModel.setTitle("Test Qubo");
+
+        QuestionBoardCreationDto quBo = ServerCommunication.createBoardRequest(quBoModel);
+
+        //Act
+        QuestionDetailsDto[] questions = ServerCommunication.retrieveQuestions(quBo.getId());
+
+        //Assert
+        assertNotNull(questions);
+        assertTrue(questions.length == 0);
+    }
+
+    //Tests if retrieveQuestions returns an array with all added questions when called through a valid board ID
+    //with questions added.
+    @Test
+    public void testRetrieveQuestionsCorrectArray() {
+        //Arrange
+        QuestionBoardCreationBindingModel quBoModel = new QuestionBoardCreationBindingModel();
+        quBoModel.setStartTime(Timestamp.from(Instant.now().truncatedTo(ChronoUnit.SECONDS)));
+        quBoModel.setTitle("Test Qubo");
+
+        QuestionBoardCreationDto quBo = ServerCommunication.createBoardRequest(quBoModel);
+        QuestionCreationDto question = ServerCommunication
+                .addQuestion(quBo.getId(), "Test Question?", "author");
+        QuestionCreationDto question2 = ServerCommunication
+                .addQuestion(quBo.getId(), "Test Question?", "author2");
+
+        UUID questionId = question.getId();
+        UUID secretCode = question.getSecretCode();
+        UUID question2Id = question2.getId();
+        UUID secretCode2 = question2.getSecretCode();
+
+        //Act
+        QuestionDetailsDto[] questions = ServerCommunication.retrieveQuestions(quBo.getId());
+
+        //Assert
+        assertNotNull(questions);
+        assertTrue(questions.length == 2);
+        assertTrue(questions[0] != questions[1]);
+        assertTrue(questions[0].getId() == questionId ^ questions[0].getId() == question2Id);
+        assertTrue(questions[1].getId() == questionId ^ questions[1].getId() == question2Id);
+        assertTrue(Arrays.asList(questions).contains(question2));
+    }
+
+    //Tests if retrieveQuestions returns null when called through an invalid board ID.
+    @Test
+    public void testRetrieveQuestions() {
+        //Arrange
+        QuestionBoardCreationBindingModel quBoModel = new QuestionBoardCreationBindingModel();
+        quBoModel.setStartTime(Timestamp.from(Instant.now().truncatedTo(ChronoUnit.SECONDS)));
+        quBoModel.setTitle("Test Qubo");
+
+        QuestionBoardCreationDto quBo = ServerCommunication.createBoardRequest(quBoModel);
+
+        //Act
+        QuestionDetailsDto[] questions = ServerCommunication.retrieveQuestions(UUID.randomUUID());
+
+        //Assert
+        assertNull(questions);
     }
 
     @Test
