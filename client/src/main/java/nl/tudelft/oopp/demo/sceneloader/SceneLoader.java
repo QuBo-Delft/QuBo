c43a218b5c0f80e944f4ce8b9a90d8282afb32af
Yunhan Wang <Y.Wang-128@student.tudelft.nl>
2021-03-23 18:01:00 +0100

    Move loadQuestionBoardCodes to sceneloader

--- client/src/main/java/nl/tudelft/oopp/demo/sceneloader/SceneLoader.java
+++ client/src/main/java/nl/tudelft/oopp/demo/sceneloader/SceneLoader.java
@@ -1,4 +1,48 @@
 package nl.tudelft.oopp.demo.sceneloader;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import nl.tudelft.oopp.demo.controllers.QuBoCodesController;
+import nl.tudelft.oopp.demo.dtos.questionboard.QuestionBoardCreationDto;
+import nl.tudelft.oopp.demo.views.AlertDialog;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.UUID;
+
 public class SceneLoader {
+
+    public static void loadQuestionBoardCodes(QuestionBoardCreationDto qd) {
+        // Create an FXMLLoader of QuBoCodes.fxml
+        FXMLLoader loader = new FXMLLoader(SceneLoader.class.getResource("/QuBoCodes.fxml"));
+
+        Parent root = null;
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Get the controller of QuBoCodes
+        QuBoCodesController controller = loader.getController();
+
+        // Transfer the data for QuBoCodes
+        controller.displayCodes(qd);
+
+        Stage stage = new Stage();
+
+        // Check if root is null
+        if (root == null) {
+            AlertDialog.display("", "Unable to display the codes");
+            return;
+        }
+
+        // Display the scene
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
 }
