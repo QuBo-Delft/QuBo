72167186bbf256f83f0f6b7b9ed0d368032a7821
Dorothy Zhang <d.l.zhang@student.tudelft.nl>
2021-03-23 20:21:03 +0100

    Add start time, add label for notifying the successful copying of codes, add return to homepage function, close current stage when loading new stage

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/QuBoCodesController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/QuBoCodesController.java
@@ -1,27 +1,50 @@
 package nl.tudelft.oopp.demo.controllers;
 
+import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 import nl.tudelft.oopp.demo.dtos.questionboard.QuestionBoardCreationDto;
+import java.io.IOException;
+import java.net.URL;
 
 public class QuBoCodesController {
-    
+
     @FXML
     private Label boardTitle;
+    @FXML
+    private Label startTime;
     @FXML
     private Label adminCode;
     @FXML
     private Label studentCode;
+    @FXML
+    private Label adminCopySuccessful;
+    @FXML
+    private Label studentCopySuccessful;
+    @FXML
+    private Button backToHome;
+
+    private final PauseTransition pause = new PauseTransition(Duration.seconds(1));
+    private Clipboard clipboard = Clipboard.getSystemClipboard();
+    private ClipboardContent content = new ClipboardContent();
+
 
     /**
      * This method aims to make a window to display the student code, the moderator code, 
-     * and the title of the question board just created.
+     * the title and start time of the question board just created.
      *
      * @param qb    The QuestionBoardCreationDto that contains the data of a question board.
      */
     public void displayCodes(QuestionBoardCreationDto qb) {
+        startTime.setText(qb.getStartTime().toString());
         boardTitle.setText(qb.getTitle());
         adminCode.setText(qb.getModeratorCode().toString());
         studentCode.setText(qb.getId().toString());
@@ -30,24 +53,49 @@ public class QuBoCodesController {
     /**
      * This method aims to copy the admin code to the user's system clipboard
      * after clicking the button for copying the admin code.
+     * Displays a message after successfully copying the code.
      */
     public void copyAdminCode() {
-        Clipboard clipboard = Clipboard.getSystemClipboard();
-        ClipboardContent content = new ClipboardContent();
         content.putString(adminCode.getText());
         clipboard.setContent(content);
 
+        if (!adminCopySuccessful.isVisible()){
+            adminCopySuccessful.setVisible(true);
+            pause.setOnFinished(e -> adminCopySuccessful.setVisible(false));
+            pause.play();
+        }
     }
 
     /**
      * This method aims to copy the student code to the user's system clipboard
      * after clicking the button for copying the student code.
+     * Displays a message after successfully copying the code.
      */
     public void copyStudentCode() {
-        Clipboard clipboard = Clipboard.getSystemClipboard();
-        ClipboardContent content = new ClipboardContent();
         content.putString(studentCode.getText());
         clipboard.setContent(content);
+
+        if(!studentCopySuccessful.isVisible()){
+            studentCopySuccessful.setVisible(true);
+            pause.setOnFinished(e -> studentCopySuccessful.setVisible(false));
+            pause.play();
+        }
+    }
+
+    /**
+     * This method takes you back to the homepage after clicking the Back to Homepage button.
+     */
+    public void backToHome() throws IOException {
+        Stage primaryStage = new Stage();
+        FXMLLoader loader = new FXMLLoader();
+        URL xmlUrl = getClass().getResource("/JoinQuBo.fxml");
+        loader.setLocation(xmlUrl);
+        Parent root = loader.load();
+
+        Stage stage = (Stage) backToHome.getScene().getWindow();
+        stage.close();
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 
 }
