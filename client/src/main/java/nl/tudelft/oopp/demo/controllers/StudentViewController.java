b63c8cc23c8e127f4e810e61eb56da14038e5654
Dorothy Zhang <d.l.zhang@student.tudelft.nl>
2021-03-23 01:24:50 +0100

    Improve UI layout, experiment with custom listview cells

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/StudentViewController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/StudentViewController.java
@@ -3,10 +3,16 @@ package nl.tudelft.oopp.demo.controllers;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
@@ -17,44 +23,53 @@ public class StudentViewController {
     @FXML
     private void initialize() {
         ObservableList<StudentViewController.CustomThing> data = FXCollections.observableArrayList();
-        data.addAll(new StudentViewController.CustomThing("Cheese", 123),
-                new StudentViewController.CustomThing("Horse", 456),
-                new StudentViewController.CustomThing("Jam", 789));
+        data.addAll(new StudentViewController.CustomThing(2, "What is life?"),
+                new StudentViewController.CustomThing(42,"Trolley problem."));
 
         questionList.setItems(data);
         questionList.setCellFactory(listView -> new CustomListCell());
     }
 
     private static class CustomThing {
-        private String name;
-        private int price;
+        private int upvoteNumber;
+        private String questionContent;
 
-        public String getName() {
-            return name;
+        public int getUpvoteNumber() {
+            return upvoteNumber;
         }
 
-        public int getPrice() {
-            return price;
+        public String getQuestionContent() {
+            return questionContent;
         }
 
-        public CustomThing(String name, int price) {
+        public CustomThing(int upvoteNumber, String questionContent) {
             super();
-            this.name = name;
-            this.price = price;
+            this.upvoteNumber = upvoteNumber;
+            this.questionContent = questionContent;
         }
     }
 
     private class CustomListCell extends ListCell<CustomThing> {
         private HBox content;
-        private Text name;
-        private Text price;
+        private Label upvoteNumber;
+        private Text questionContent;
 
         public CustomListCell() {
             super();
-            name = new Text();
-            price = new Text();
-            VBox vbox = new VBox(name, price);
-            content = new HBox(new Label("[Graphic]"), vbox);
+            upvoteNumber = new Label();
+            questionContent = new Text();
+
+            Button upvoteTriangle = new Button("up");
+            VBox upvote = new VBox(upvoteTriangle, upvoteNumber);
+            upvote.setAlignment(Pos.CENTER);
+
+            MenuButton options = new MenuButton();
+            options.getItems().addAll(new MenuItem("Edit"), new MenuItem("Delete"));
+
+            Pane pane = new Pane();
+            HBox.setHgrow(pane, Priority.ALWAYS);
+
+            content = new HBox(upvote, questionContent, pane, options);
             content.setSpacing(10);
         }
 
@@ -62,8 +77,8 @@ public class StudentViewController {
         protected void updateItem(StudentViewController.CustomThing item, boolean empty) {
             super.updateItem(item, empty);
             if (item != null && !empty) { // <== test for null item and empty parameter
-                name.setText(item.getName());
-                price.setText(String.format("%d $", item.getPrice()));
+                upvoteNumber.setText(Integer.toString(item.getUpvoteNumber()));
+                questionContent.setText(item.getQuestionContent());
                 setGraphic(content);
             } else {
                 setGraphic(null);
