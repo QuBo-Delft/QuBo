995c8aaafa16ced8f3e47ce6a68ce4cefd2a824a
Cassandra Visser <C.M.S.Visser@student.tudelft.nl>
2021-03-23 22:11:45 +0100

    Add tests for createBoardRequest Adds one test that tests the method with a valid QuestionBoardCreationBindingModel, and one that tests it with an invalid object.

--- client/src/test/java/nl/tudelft/oopp/demo/communication/ServerCommunicationTest.java
+++ client/src/test/java/nl/tudelft/oopp/demo/communication/ServerCommunicationTest.java
@@ -1,14 +1,49 @@
 package nl.tudelft.oopp.demo.communication;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
+import nl.tudelft.oopp.demo.dtos.questionboard.QuestionBoardCreationBindingModel;
+import nl.tudelft.oopp.demo.dtos.questionboard.QuestionBoardCreationDto;
 import org.junit.jupiter.api.Test;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 
 public class ServerCommunicationTest {
 
+    //Tests if the createBoardRequest method returns a QuestionBoardCreationDto with the same
+    //attribute values as the QuestionBoardCreationBindingModel used to call the method with.
+    @Test
+    public void testCreateBoardRequestValidRequest() {
+        //Arrange
+        QuestionBoardCreationBindingModel quBoModel = new QuestionBoardCreationBindingModel();
+        quBoModel.setStartTime(Timestamp.from(Instant.now().truncatedTo(ChronoUnit.SECONDS)));
+        quBoModel.setTitle("Test Qubo");
+
+        //Act
+        QuestionBoardCreationDto quBo = ServerCommunication.createBoardRequest(quBoModel);
+
+        //Assert
+        assertEquals(quBoModel.getStartTime(), quBo.getStartTime());
+        assertEquals(quBoModel.getTitle(), quBo.getTitle());
+    }
+
+    //Tests if the createBoardRequest method returns null when called without a correct
+    //QuestionBoardCreationBindingModel.
+    @Test
+    public void testCreateBoardRequestInvalidRequest() {
+        //Act
+        QuestionBoardCreationDto quBo = ServerCommunication.createBoardRequest(null);
+
+        //Assert
+        assertNull(quBo);
+    }
+
     @Test
     public void testRandomQuote() {
+
         assertNotNull(ServerCommunication.getQuote());
     }
 }
