4730245caab0095c5b69cc349002cf516735b877
Dorothy Zhang <d.l.zhang@student.tudelft.nl>
2021-03-25 20:37:47 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/StudentViewController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/StudentViewController.java
@@ -156,7 +156,7 @@ public class StudentViewController {
      * Method that runs when the Leave button is clicked.
      * Pops up a confirmation dialogue.
      * If the user clicks yes -> Question board closes and user returns to the JoinQuBo page
-     * If user clicks no -> Confirmation dialogue closes and user returns to the question board
+     * If the user clicks no -> Confirmation dialogue closes and user returns to the question board
      */
     public void leaveQuBo() {
         boolean backHome = ConfirmationDialog.display("Leave Question Board?",
