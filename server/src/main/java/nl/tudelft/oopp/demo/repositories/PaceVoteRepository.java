fa768cfeeddf6db0879547a943512996c27d3f8e
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-03-27 10:47:18 +0200

    Add countByQuestionBoardAndPaceType in PaceVoteRepository

--- server/src/main/java/nl/tudelft/oopp/demo/repositories/PaceVoteRepository.java
+++ server/src/main/java/nl/tudelft/oopp/demo/repositories/PaceVoteRepository.java
@@ -2,14 +2,12 @@ package nl.tudelft.oopp.demo.repositories;
 
 import java.util.Set;
 import java.util.UUID;
-
+import javax.transaction.Transactional;
+import nl.tudelft.oopp.demo.entities.PaceType;
 import nl.tudelft.oopp.demo.entities.PaceVote;
-import nl.tudelft.oopp.demo.entities.Question;
 import nl.tudelft.oopp.demo.entities.QuestionBoard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import javax.transaction.Transactional;
 
 @Repository("PaceVoteRepository")
 public interface PaceVoteRepository extends JpaRepository<PaceVote, UUID> {
@@ -19,4 +17,6 @@ public interface PaceVoteRepository extends JpaRepository<PaceVote, UUID> {
 
     @Transactional
     void deletePaceVoteById(UUID id);
+
+    int countByQuestionBoardAndPaceType(QuestionBoard board, PaceType paceType);
 }
