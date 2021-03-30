95188156847912ad576838b70ca5109a147ce5d8
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-03-30 22:02:41 +0300

    Add tests for AnswerService

---
+++ server/src/test/java/nl/tudelft/oopp/qubo/services/AnswerServiceTests.java
@@ -0,0 +1,69 @@
+package nl.tudelft.oopp.qubo.services;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+import nl.tudelft.oopp.qubo.dtos.answer.AnswerCreationBindingModel;
+import nl.tudelft.oopp.qubo.entities.Answer;
+import nl.tudelft.oopp.qubo.entities.Question;
+import nl.tudelft.oopp.qubo.entities.QuestionBoard;
+import nl.tudelft.oopp.qubo.repositories.AnswerRepository;
+import nl.tudelft.oopp.qubo.repositories.QuestionBoardRepository;
+import nl.tudelft.oopp.qubo.repositories.QuestionRepository;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class AnswerServiceTests {
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Autowired
+    private QuestionBoardRepository questionBoardRepository;
+
+    @Autowired
+    private AnswerService answerService;
+
+    @Test
+    public void addAnswerToQuestion_withValidQuestion_worksCorrectly() {
+        // Arrange
+        QuestionBoard board = new QuestionBoard();
+        board.setModeratorCode(UUID.randomUUID());
+        board.setStartTime(Timestamp.from(Instant.now()));
+        board.setTitle("Test board");
+        questionBoardRepository.save(board);
+
+        Question question = new Question();
+        question.setAuthorName("Author");
+        question.setText("Test question");
+        question.setSecretCode(UUID.randomUUID());
+        question.setTimestamp(Timestamp.from(Instant.now()));
+        question.setQuestionBoard(board);
+        questionRepository.save(question);
+
+        AnswerCreationBindingModel model = new AnswerCreationBindingModel();
+        model.setText("Test answer");
+
+        // Act
+        Answer result = answerService.addAnswerToQuestion(model, question);
+
+        // Assert
+        assertNotNull(result);
+        List<Answer> inDb = answerRepository.findAll();
+        assertEquals(1, inDb.size());
+        assertEquals(inDb.get(0).getId(), result.getId());
+        assertEquals(inDb.get(0).getText(), result.getText());
+        assertEquals(model.getText(), result.getText());
+        assertEquals(question.getId(), result.getQuestion().getId());
+    }
+}
