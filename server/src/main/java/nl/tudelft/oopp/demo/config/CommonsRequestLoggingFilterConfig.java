5f5df1bdff38df2758623f4c15baf7bbab181995
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-03-30 11:14:52 +0300

    Configure CommonsRequestLoggingFilter to log all requests

---
+++ server/src/main/java/nl/tudelft/oopp/demo/config/CommonsRequestLoggingFilterConfig.java
@@ -0,0 +1,24 @@
+package nl.tudelft.oopp.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class CommonsRequestLoggingFilterConfig {
+
+    /**
+     * Configures the CommonsRequestLoggingFilter.
+     *
+     * @return The newly-created CommonsRequestLoggingFilter.
+     */
+    @Bean
+    public CommonsRequestLoggingFilter getCommonsRequestLoggingFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(3000);
+        filter.setIncludeClientInfo(true);
+        return filter;
+    }
+}
