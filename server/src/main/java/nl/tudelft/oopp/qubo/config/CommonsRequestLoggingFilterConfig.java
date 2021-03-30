25b9a76470ea1f016c3d5cbea1a5cc787dd51701
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-03-30 12:12:17 +0300

    Fix package name of CommonsRequestLoggingFilterConfig

--- server/src/main/java/nl/tudelft/oopp/qubo/config/CommonsRequestLoggingFilterConfig.java
+++ server/src/main/java/nl/tudelft/oopp/qubo/config/CommonsRequestLoggingFilterConfig.java
@@ -1,4 +1,4 @@
-package nl.tudelft.oopp.demo.config;
+package nl.tudelft.oopp.qubo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
