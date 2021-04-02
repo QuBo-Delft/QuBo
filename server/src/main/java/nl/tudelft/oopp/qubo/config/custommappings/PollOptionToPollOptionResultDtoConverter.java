c3eecc912e1df91b92adb8a64678b92d4b879847
Yunhan Wang <Y.Wang-128@student.tudelft.nl>
2021-04-02 16:57:35 +0200

    Add PollOptionToPollOptionResultDtoConverter

--- server/src/main/java/nl/tudelft/oopp/qubo/config/custommappings/PollOptionToPollOptionResultDtoConverter.java
+++ server/src/main/java/nl/tudelft/oopp/qubo/config/custommappings/PollOptionToPollOptionResultDtoConverter.java
@@ -1,2 +1,36 @@
-package nl.tudelft.oopp.qubo.config.custommappings;public class PollOptionToPollOptionResultDtoConverter {
+package nl.tudelft.oopp.qubo.config.custommappings;
+
+import nl.tudelft.oopp.qubo.dtos.polloption.PollOptionResultDto;
+import nl.tudelft.oopp.qubo.entities.PollOption;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class PollOptionToPollOptionResultDtoConverter {
+    private final ModelMapper mapper;
+
+    /**
+     * Initialise PollOptionToPollOptionResultDtoConverter.
+     * @param modelMapper   The modelMapper object.
+     */
+    public PollOptionToPollOptionResultDtoConverter(ModelMapper modelMapper) {
+        this.mapper = modelMapper;
+    }
+
+    /**
+     * Create custom mapping for converting from PollOption to PollOptionResultDto.
+     * Set the "votes" attribute to the amount of poll votes.
+     */
+    @PostConstruct
+    public void init() {
+        mapper.addMappings(new PropertyMap<PollOption, PollOptionResultDto>() {
+            @Override
+            protected void configure() {
+                map().setVotes(source.getVotes().size());
+            }
+        });
+    }
 }
