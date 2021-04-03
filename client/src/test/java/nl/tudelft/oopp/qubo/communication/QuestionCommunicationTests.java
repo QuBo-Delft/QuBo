c33f0d5f8c27b417fdb21670929bea49e36cf405
Yunhan Wang <Y.Wang-128@student.tudelft.nl>
2021-04-03 13:28:26 +0200

    Move tests to QuestionCommunicationTests and delete tests in ServerCommunicationTests

--- client/src/test/java/nl/tudelft/oopp/qubo/communication/QuestionCommunicationTests.java
+++ client/src/test/java/nl/tudelft/oopp/qubo/communication/QuestionCommunicationTests.java
@@ -36,6 +36,234 @@ public class QuestionCommunicationTests {
         httpClientMock = new HttpClientMock();
     }
 
+    // Test if the addQuestion method returns a non-null response body after being called
+    // with a valid board ID, and receiving statusCode 200.
+    @Test
+    public void testAddQuestionValidBoard() {
+        // Arrange
+        ServerCommunication.setClient(httpClientMock);
+        httpClientMock.onPost(subUrl + "api/board/" + uuid1 + "/question")
+                .doReturnStatus(200);
+        // Act
+        String responseBody = QuestionCommunication
+                .addQuestion(uuid1, "Why is CO so confusing?", "Koen");
+
+        // Assert
+        assertNotNull(responseBody);
+        // Verify if the request was truly made
+        httpClientMock.verify()
+                .post(subUrl + "api/board/" + uuid1 + "/question").called();
+    }
+
+    // Test if the addQuestion method returns null after being called with an invalid
+    // questionId and moderator code, and receiving statusCode 404 and failureToken as
+    // the response body.
+    @Test
+    public void testAddQuestionInvalidBoard() {
+        // Arrange
+        ServerCommunication.setClient(httpClientMock);
+        httpClientMock.onPost(subUrl + "api/board/" + uuid1 + "/question")
+                .doReturnStatus(404).doReturn(failureToken);
+        // Act
+        String responseBody = QuestionCommunication
+                .addQuestion(uuid1, "Why is CO so confusing?", "Koen");
+
+        // Assert
+        assertNull(responseBody);
+        // Verify if the request was truly made
+        httpClientMock.verify()
+                .post(subUrl + "api/board/" + uuid1 + "/question").called();
+    }
+
+    // Test if the addQuestion method returns the successToken after being called
+    // with a valid questionId and moderator code, and receiving statusCode 200 and the successToken
+    // as the response body.
+    @Test
+    public void testAddQuestionGivingCorrectResponseBody() {
+        // Arrange
+        ServerCommunication.setClient(httpClientMock);
+        httpClientMock.onPost(subUrl + "api/board/" + uuid1 + "/question")
+                .doReturn(successToken);
+        // Act
+        String responseBody = QuestionCommunication
+                .addQuestion(uuid1, "Why is CO so confusing?", "Koen");
+
+        //Assert
+        assertEquals(successToken, responseBody);
+        // Verify if the request was truly made
+        httpClientMock.verify()
+                .post(subUrl + "api/board/" + uuid1 + "/question").called();
+    }
+
+    // Test if the editQuestion method returns a non-null response body after being called
+    // with a valid questionId and moderator code and receiving statusCode 200.
+    @Test
+    public void testEditQuestionWithValidCode() {
+        // Arrange
+        ServerCommunication.setClient(httpClientMock);
+        httpClientMock.onPut(subUrl + "api/question/" + uuid1 + "?code=" + uuid2)
+                .doReturnStatus(200);
+        // Act
+        String responseBody = QuestionCommunication
+                .editQuestion(uuid1, uuid2, "Why is CO taught so clear");
+
+        // Assert
+        assertNotNull(responseBody);
+        // Verify if the request was truly made
+        httpClientMock.verify()
+                .put(subUrl + "api/question/" + uuid1 + "?code=" + uuid2).called();
+    }
+
+    // Test if the editQuestion method returns null after being called with an invalid
+    // questionId and moderator code and receiving statusCode 404 and failureToken as the
+    // response body.
+    @Test
+    public void testEditQuestionWithInvalidCode() {
+        // Arrange
+        ServerCommunication.setClient(httpClientMock);
+        httpClientMock.onPut(subUrl + "api/question/" + uuid1 + "?code=" + uuid3)
+                .doReturnStatus(404).doReturn(failureToken);
+        // Act
+        String responseBody = QuestionCommunication
+                .editQuestion(uuid1, uuid3, "Why is CO so clear");
+
+        // Assert
+        assertNull(responseBody);
+        // Verify if the request was truly made
+        httpClientMock.verify()
+                .put(subUrl + "api/question/" + uuid1 + "?code=" + uuid3).called();
+    }
+
+    // Test if the editQuestion method returns the successToken after being called
+    // with a valid questionId and moderator code and receiving statusCode 200 and
+    // successToken as the response body.
+    @Test
+    public void testEditQuestionGivingCorrectResponseBody() {
+        // Arrange
+        ServerCommunication.setClient(httpClientMock);
+        httpClientMock.onPut(subUrl + "api/question/" + uuid1 + "?code=" + uuid2)
+                .doReturn(successToken);
+        // Act
+        String responseBody = QuestionCommunication
+                .editQuestion(uuid1, uuid2, "Why is CO so clear");
+
+        // Assert
+        assertEquals(successToken, responseBody);
+        // Verify if the request was truly made
+        httpClientMock.verify()
+                .put(subUrl + "api/question/" + uuid1 + "?code=" + uuid2).called();
+    }
+
+    // Test if the deleteQuestion method returns a non-null response body after being called
+    // with a valid questionId and moderator code and receiving statusCode 200.
+    @Test
+    public void testDeleteQuestionValidRequest() {
+        // Arrange
+        ServerCommunication.setClient(httpClientMock);
+        httpClientMock.onDelete(subUrl + "api/question/" + uuid1 + "?code=" + uuid2)
+                .doReturnStatus(200);
+        // Act
+        String responseBody = QuestionCommunication.deleteQuestion(uuid1, uuid2);
+
+        // Assert
+        assertNotNull(responseBody);
+        // Verify if the request was truly made
+        httpClientMock.verify()
+                .delete(subUrl + "api/question/" + uuid1 + "?code=" + uuid2).called();
+    }
+
+    // Test if the deleteQuestion method returns null after being called with an invalid
+    // questionId and moderator code and receiving statusCode 404 and failureToken as the response body.
+    @Test
+    public void testDeleteQuestionInvalidQuBo() {
+        // Arrange
+        ServerCommunication.setClient(httpClientMock);
+        httpClientMock.onDelete(subUrl + "api/question/" + uuid1 + "?code=" + uuid2)
+                .doReturnStatus(404).doReturn(failureToken);
+        // Act
+        String responseBody = QuestionCommunication.deleteQuestion(uuid1, uuid2);
+
+        // Assert
+        assertNull(responseBody);
+        // Verify if the request was truly made
+        httpClientMock.verify()
+                .delete(subUrl + "api/question/" + uuid1 + "?code=" + uuid2).called();
+    }
+
+    // Test if the deleteQuestion method returns the successToken after being called
+    // with a valid questionId and moderator code and receiving statusCode 200 and the successToken
+    // as the response body.
+    @Test
+    public void testDeleteQuestionGivingCorrectResponseBody() {
+        // Arrange
+        ServerCommunication.setClient(httpClientMock);
+        httpClientMock.onDelete(subUrl + "api/question/" + uuid1 + "?code=" + uuid2)
+                .doReturn(successToken);
+        // Act
+        String responseBody = QuestionCommunication.deleteQuestion(uuid1, uuid2);
+
+        // Assert
+        assertEquals(successToken, responseBody);
+        // Verify if the request was truly made
+        httpClientMock.verify()
+                .delete(subUrl + "api/question/" + uuid1 + "?code=" + uuid2).called();
+    }
+
+    // Test if the markQuestionAsAnswered method returns a non-null response body after being called
+    // with a valid questionId and moderator code and receiving statusCode 200.
+    @Test
+    public void testMarkQuestionAsAnsweredThroughValidRequest() {
+        // Arrange
+        ServerCommunication.setClient(httpClientMock);
+        httpClientMock.onPatch(subUrl + "api/question/" + uuid1 + "/answer?code=" + uuid2)
+                .doReturnStatus(200);
+        // Act
+        String responseBody = QuestionCommunication.markQuestionAsAnswered(uuid1, uuid2);
+
+        // Assert
+        assertNotNull(responseBody);
+        // Verify if the request was truly made
+        httpClientMock.verify()
+                .patch(subUrl + "api/question/" + uuid1 + "/answer?code=" + uuid2).called();
+    }
+
+    // Test if the markQuestionAsAnswered method returns null after being called with an invalid
+    // questionId and moderator code and receiving statusCode 404 and failureToken as the response body.
+    @Test
+    public void testMarkQuestionAsAnsweredThroughInvalidRequest() {
+        // Arrange
+        ServerCommunication.setClient(httpClientMock);
+        httpClientMock.onPatch(subUrl + "api/question/" + uuid1 + "/answer?code=" + uuid2)
+                .doReturnStatus(404).doReturn(failureToken);
+        // Act
+        String responseBody = QuestionCommunication.markQuestionAsAnswered(uuid1, uuid2);
+
+        // Assert
+        assertNull(responseBody);
+        // Verify if the request was truly made
+        httpClientMock.verify()
+                .patch(subUrl + "api/question/" + uuid1 + "/answer?code=" + uuid2).called();
+    }
+
+    // Test if the markQuestionAsAnswered method returns the successToken after being called
+    // with a valid questionId and moderator code and receiving statusCode 200 and the successToken
+    // as the response body.
+    @Test
+    public void testMarkQuestionAsAnsweredGivingCorrectResponseBody() {
+        // Arrange
+        ServerCommunication.setClient(httpClientMock);
+        httpClientMock.onPatch(subUrl + "api/question/" + uuid1 + "/answer?code=" + uuid2)
+                .doReturn(successToken);
+        // Act
+        String responseBody = QuestionCommunication.markQuestionAsAnswered(uuid1, uuid2);
+
+        // Assert
+        assertEquals(successToken, responseBody);
+        // Verify if the request was truly made
+        httpClientMock.verify()
+                .patch(subUrl + "api/question/" + uuid1 + "/answer?code=" + uuid2).called();
+    }
+
     // Test if the addAnswer method returns a non-null response body after being called with a
     // valid question ID and moderator code then receiving status code 200.
     @Test
