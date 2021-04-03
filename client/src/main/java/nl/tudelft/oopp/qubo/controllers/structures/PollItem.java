f150c67329563e80940d6edd6b8f6cfbf954517c
Cassandra Visser <C.M.S.Visser@student.tudelft.nl>
2021-04-04 00:48:20 +0200

    Add a PollItem class to construct a gridpane to hold polls and their options.

---
+++ client/src/main/java/nl/tudelft/oopp/qubo/controllers/structures/PollItem.java
@@ -0,0 +1,123 @@
+package nl.tudelft.oopp.qubo.controllers.structures;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.RadioMenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+import nl.tudelft.oopp.qubo.dtos.polloption.PollOptionDetailsDto;
+
+import java.util.Set;
+
+public class PollItem extends GridPane {
+    private GridPane pollPane;
+    private Text pollQuestion;
+    private VBox pollVbox;
+
+    private Set<PollOptionDetailsDto> options;
+
+    private ScrollPane pollScPane;
+    private VBox pollContainer;
+
+    /**
+     * Constructs a new PollItem.
+     *
+     * @param text          The text that should be associated with the poll.
+     * @param pollOptions   The options that students can select.
+     * @param pollContainer The VBox containing the poll.
+     * @param scrollPane    ScrollPane containing the VBox that contains the list of questions
+     */
+    public PollItem(String text, Set<PollOptionDetailsDto> pollOptions, VBox pollContainer,
+                        ScrollPane scrollPane) {
+        this.pollQuestion = new Text(text);
+        this.options = pollOptions;
+
+        this.pollContainer = pollContainer;
+        pollScPane = scrollPane;
+
+        construct();
+    }
+
+    /**
+     * This method constructs the gridpane that will contain the poll and its options.
+     */
+    private void construct() {
+        //Bind the managed property to the visible property so that the node is not accounted for
+        //in the layout when it is not visible.
+        pollQuestion.managedProperty().bind(pollQuestion.visibleProperty());
+
+        //Set padding for the cell that will contain the Poll.
+        this.setPadding(new Insets(0,10,20,0));
+
+        //Construct a new questionPane to hold the question and add to content pane
+        pollPane = newPollPane();
+        this.addRow(0, pollPane);
+
+        //Add the poll options
+        addOptions();
+
+        this.setGridLinesVisible(true);
+    }
+
+    /**
+     * This method adds the poll options to the GridPane containing the poll.
+     */
+    private void addOptions() {
+        MenuBar bar = new MenuBar();
+        Menu optionsMenu = new Menu();
+
+        //Set up the toggle group to which all poll options will be bound
+        ToggleGroup optionGroup = new ToggleGroup();
+
+        //Add all PollOptionDetailsDtos to the menu
+        for (PollOptionDetailsDto option : options) {
+            //Create a new radio button for the poll option and add it to the menu
+            RadioMenuItem optionButton = new RadioMenuItem(option.getOptionText());
+            optionButton.setToggleGroup(optionGroup);
+            optionsMenu.getItems().add(optionButton);
+        }
+
+        //Add the menu to a MenuBar
+        bar.getMenus().add(optionsMenu);
+
+        //Add the menubar to a VBox
+        VBox pollOptionBox = new VBox();
+        pollOptionBox.getChildren().add(bar);
+        pollOptionBox.setPadding(new Insets(10,15,10,15));
+
+        //Add the option box to the PollItem GridPane
+        this.addRow(1, pollOptionBox);
+    }
+
+    /**
+     * This method constructs a new pollPane to hold the poll text and poll options.
+     *
+     * @return A GridPane containing all poll information needed by the student.
+     */
+    private GridPane newPollPane() {
+        GridPane gridpane = new GridPane();
+
+        //Add the poll question to the gridpane and make sure that the poll question text does not overflow
+        gridpane.addColumn(1, pollQuestion);
+        pollQuestion.wrappingWidthProperty().bind(pollScPane.widthProperty().subtract(180));
+
+        //Set column constraints
+        ColumnConstraints col2 = new ColumnConstraints();
+        col2.setHgrow(Priority.ALWAYS);
+        gridpane.getColumnConstraints().addAll(new ColumnConstraints(50), col2,
+                new ColumnConstraints(50));
+
+        //Set padding
+        gridpane.setPadding(new Insets(10,3,5,3));
+
+        return gridpane;
+    }
+
+}
