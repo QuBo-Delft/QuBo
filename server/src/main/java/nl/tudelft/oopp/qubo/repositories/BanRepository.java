dee9d1999558716856b3d317c6feb5f8aeb94184
Jort van Leenen <J.P.vanLeenen@student.tudelft.nl>
2021-04-04 17:08:23 +0200

    Add tests for the banRepository This adds tests for the banRepository and also adds two methods for easy testing in the BanRepository.

--- server/src/main/java/nl/tudelft/oopp/qubo/repositories/BanRepository.java
+++ server/src/main/java/nl/tudelft/oopp/qubo/repositories/BanRepository.java
@@ -4,8 +4,13 @@ import nl.tudelft.oopp.qubo.entities.Ban;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.UUID;
 
 @Repository("BanRepository")
 public interface BanRepository extends JpaRepository<Ban, UUID> {
+    Ban getById(UUID banId);
+
+    @Transactional
+    void deleteBanById(UUID banId);
 }
