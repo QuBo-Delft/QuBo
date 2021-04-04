6474d52e4dc083908feb6e3c3f4fbc3b3a89a67d
Cassandra Visser <C.M.S.Visser@student.tudelft.nl>
2021-04-04 18:11:22 +0200

    Change the error message display of JoinQuBo The user name error message is updated when a user types, but the question board error message takes precedence and is only changed when the corresponding board is found.

--- client/src/main/java/nl/tudelft/oopp/qubo/controllers/JoinQuBoController.java
+++ client/src/main/java/nl/tudelft/oopp/qubo/controllers/JoinQuBoController.java
@@ -9,12 +9,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 import nl.tudelft.oopp.qubo.communication.QuestionBoardCommunication;
-import nl.tudelft.oopp.qubo.communication.ServerCommunication;
 import nl.tudelft.oopp.qubo.dtos.questionboard.QuestionBoardDetailsDto;
 import nl.tudelft.oopp.qubo.sceneloader.SceneLoader;
-import nl.tudelft.oopp.qubo.views.ConfirmationDialog;
 
 import java.util.UUID;
 
@@ -42,6 +39,10 @@ public class JoinQuBoController {
             .setDateFormat("yyyy-MM-dd'T'HH:mm:ssX")
             .create();
 
+    private static final String quBoError = "Error: Could not find the requested question board!\nPlease "
+            + "check if you inserted the code correctly!";
+    private static final String userNameError = "Error: No username was entered. Please enter a username";
+
     /**
      * Method that handles mouse click interaction with the create question board button.
      *
@@ -50,7 +51,7 @@ public class JoinQuBoController {
     @FXML
     void createButtonClicked(ActionEvent event) {
         // Get the stage that is currently displayed on-screen
-        Stage currentStage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
         //Load the create question board page
         SceneLoader.defaultLoader(currentStage, "CreateQuBo");
@@ -67,32 +68,31 @@ public class JoinQuBoController {
         String qbCode = questionBoardCode.getText();
         UUID boardCode;
 
-        // We check whether the user input is a UUID and show the error message label when this is not the case.
+        // We check whether the user input is a UUID and show the corresponding error when this is not the case.
         try {
             boardCode = UUID.fromString(qbCode);
         } catch (IllegalArgumentException exception) {
-            errorMessageLabel.setVisible(true);
+            setErrorMessageLabel(true);
             return;
         }
 
         String user = userName.getText();
-        // Check if the user entered a user name.
+        // Check if the user entered a user name and show the corresponding error when this is not the case.
         if (user.length() == 0) {
-            errorMessageLabel.setText("Error: No username was entered. Please enter a username");
-            errorMessageLabel.setVisible(true);
+            setErrorMessageLabel(false);
             return;
         }
 
         String resBody = QuestionBoardCommunication.retrieveBoardDetails(boardCode);
 
-        // We check whether the question board exists; If not show the error message label.
+        // We check whether the question board exists; If not show the corresponding error message.
         if (resBody == null) {
-            errorMessageLabel.setVisible(true);
+            setErrorMessageLabel(true);
             return;
         }
 
         QuestionBoardDetailsDto questionBoard = gson.fromJson(resBody, QuestionBoardDetailsDto.class);
-        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
         //Load the student view if the code entered by the user was the board ID of the question board.
         //Load the moderator view if this is not the case.
@@ -104,20 +104,40 @@ public class JoinQuBoController {
     }
 
     /**
-     * Method that handles text field input changes.
+     * Method that handles text field input changes of the text field where a user types their name.
      */
     @FXML
-    public void textHandler() {
-        if (errorMessageLabel.isVisible() && !errorMessageLabel.getText()
-                .equals("Error: No username was entered. Please enter a username")) {
-            try {
-                UUID boardCode = UUID.fromString(questionBoardCode.getText());
-            } catch (IllegalArgumentException exception) {
-                return;
-            }
+    public void userTextHandler() {
+        String userNameText = userName.getText();
+
+        //Do not change the error message if it is displaying the question board error message as this
+        //should only be changed if the question board exists, which is checked when the join button is
+        //pressed.
+        if (errorMessageLabel.getText().equals(quBoError) && errorMessageLabel.isVisible()) {
+            return;
+        //Set the error message to the user name error if no username was entered
+        } else if (userNameText.equals("")) {
+            setErrorMessageLabel(false);
+        //If the user name error was visible when this method was called, a character must have been typed
+        //in the user name text field. Set the visibility of the error to false.
+        } else if (errorMessageLabel.getText().equals(userNameError)) {
             errorMessageLabel.setVisible(false);
-        } else if (errorMessageLabel.isVisible()) {
-            errorMessageLabel.setVisible(userName.getText().length() > 0);
         }
+    }
+
+    /**
+     * This method sets the error message label's text to the quBoError if the passed boolean is true and sets
+     * it to the userNameError otherwise. It also sets its visibility to true.
+     *
+     * @param isQuBoMessage The boolean that decides what error message is shown.
+     */
+    private void setErrorMessageLabel(boolean isQuBoMessage) {
+        if (isQuBoMessage) {
+            errorMessageLabel.setText(quBoError);
+        } else {
+            errorMessageLabel.setText(userNameError);
+        }
+
+        errorMessageLabel.setVisible(true);
     }
 }
