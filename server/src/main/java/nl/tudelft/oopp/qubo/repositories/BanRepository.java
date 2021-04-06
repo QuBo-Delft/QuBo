c66f5593286cd89dd9c765ba70b19daea3cb066f
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-04-06 19:10:23 +0300

    Change getBanByQuestionBoard to getBanByQuestionBoardAndIp in BanRepository

--- server/src/main/java/nl/tudelft/oopp/qubo/repositories/BanRepository.java
+++ server/src/main/java/nl/tudelft/oopp/qubo/repositories/BanRepository.java
@@ -1,15 +1,12 @@
 package nl.tudelft.oopp.qubo.repositories;
 
+import java.util.Set;
 import java.util.UUID;
 import javax.transaction.Transactional;
 import nl.tudelft.oopp.qubo.entities.Ban;
 import nl.tudelft.oopp.qubo.entities.QuestionBoard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Set;
-import javax.transaction.Transactional;
-import java.util.UUID;
 
 /**
  * The Ban repository.
@@ -20,10 +17,25 @@ public interface BanRepository extends JpaRepository<Ban, UUID> {
      * Get ban by id.
      *
      * @param banId The ban id.
-     * @return The by id.
+     * @return The ban if it exists.
      */
     Ban getById(UUID banId);
 
+    /**
+     * Get ban by question board and ip.
+     *
+     * @param questionBoard Question board.
+     * @param ip            The IP address.
+     * @return The ban if it exists.
+     */
+    Ban getBanByQuestionBoardAndIp(QuestionBoard questionBoard, String ip);
+
+    /**
+     * Get all bans by question board.
+     *
+     * @param questionBoard Question board.
+     * @return The set of bans.
+     */
     Set<Ban> getBanByQuestionBoard(QuestionBoard questionBoard);
 
     /**
