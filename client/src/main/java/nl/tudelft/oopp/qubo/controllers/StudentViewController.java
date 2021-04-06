3c28de863e0670bd997a3fc4d2a777978218a9b9
Jort van Leenen <J.P.vanLeenen@student.tudelft.nl>
2021-04-06 11:34:26 +0200

    Fix error dialog pace vote on close This fixes a bug where an error message would be displayed when deleting a pace vote upon closing a stage/scene. A parameter has been added to determine whether to show a dialog on error or not.

--- client/src/main/java/nl/tudelft/oopp/qubo/controllers/StudentViewController.java
+++ client/src/main/java/nl/tudelft/oopp/qubo/controllers/StudentViewController.java
@@ -256,7 +256,7 @@ public class StudentViewController {
         // first remove the old pace vote before creating a new one
         if (paceVoteCreationDto != null) {
             // If deletion fails, reset the radio button to its state before the call to this method and return
-            if (!deletePaceVote()) {
+            if (!deletePaceVote(false)) {
                 pace.selectToggle(previouslyPressed);
                 // Allow input again, as processing the pace vote is completed
                 paceVbox.setDisable(false);
@@ -276,11 +276,15 @@ public class StudentViewController {
      * Gets called by the paceVoteHandler and upon closing the stage, either through the sidebar or by
      * making use of the close button of the stage. It removes a set pace vote.
      *
-     * @return True or false depending on whether the removal was successful.
+     * @param noDialogOnError  A boolean value that decides whether an error dialog is shown on error.
+     * @return          True or false depending on whether the removal was successful.
      */
-    public boolean deletePaceVote() {
+    public boolean deletePaceVote(boolean noDialogOnError) {
         String resBody = PaceVoteCommunication.deletePaceVote(quBo.getId(), paceVoteCreationDto.getId());
         if (resBody == null) {
+            if (noDialogOnError) {
+                return false;
+            }
             AlertDialog.display("Unsuccessful Request",
                 "Failed to change your pace vote, please try again.");
             return false;
@@ -414,7 +418,7 @@ public class StudentViewController {
         if (backHome) {
             // If a pace vote is set upon leaving the Question Board, remove the pace vote when the user leaves
             if (paceVoteCreationDto != null) {
-                deletePaceVote();
+                deletePaceVote(true);
             }
             SceneLoader.defaultLoader((Stage) leaveQuBo.getScene().getWindow(), "JoinQuBo");
         }
