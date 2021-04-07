3bc3c983bf7500a82f148098c26603278f7b988f
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-04-08 02:06:58 +0300

    Implement QuestionToStringConverter with a convertToString method

---
+++ client/src/main/java/nl/tudelft/oopp/qubo/utilities/QuestionToStringConverter.java
@@ -0,0 +1,50 @@
+package nl.tudelft.oopp.qubo.utilities;
+
+import java.text.SimpleDateFormat;
+import nl.tudelft.oopp.qubo.dtos.answer.AnswerDetailsDto;
+import nl.tudelft.oopp.qubo.dtos.question.QuestionDetailsDto;
+import nl.tudelft.oopp.qubo.utilities.sorting.Sorting;
+
+/**
+ * An utility class for converting questions into a formatted string.
+ */
+public class QuestionToStringConverter {
+    /**
+     * Converts questions into a string.
+     *
+     * @param questions The array of questions to be converted.
+     * @return The output string.
+     */
+    public static String convertToString(QuestionDetailsDto[] questions) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Questions:\n\n");
+
+        Sorting.sortOnTimestamp(questions);
+
+        var dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+        for (QuestionDetailsDto q : questions) {
+            sb.append('[');
+            sb.append(dateFormat.format(q.getTimestamp()));
+            sb.append("] ");
+            sb.append(q.getAuthorName());
+            sb.append(": (");
+            sb.append(q.getUpvotes());
+            sb.append("↑)\n  ");
+            sb.append(q.getText());
+            sb.append('\n');
+
+            for (AnswerDetailsDto a : q.getAnswers()) {
+                sb.append("    - ");
+                sb.append(a.getText());
+                sb.append(" [");
+                sb.append(dateFormat.format(a.getTimestamp()));
+                sb.append("]\n");
+            }
+
+            sb.append('\n');
+        }
+
+        return sb.toString();
+    }
+}
