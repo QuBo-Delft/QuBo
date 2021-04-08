71b28f13290607c11b2f6a7bfa2b7cecb6fc9bf6
Jort van Leenen <J.P.vanLeenen@student.tudelft.nl>
2021-04-08 14:46:51 +0200

    Prepare Moderator and Student View testing

--- client/src/test/java/nl/tudelft/oopp/qubo/controllers/ModeratorViewControllerTest.java
+++ client/src/test/java/nl/tudelft/oopp/qubo/controllers/ModeratorViewControllerTest.java
@@ -1,13 +1,9 @@
 package nl.tudelft.oopp.qubo.controllers;
 
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-import nl.tudelft.oopp.qubo.dtos.questionboard.QuestionBoardCreationDto;
 import nl.tudelft.oopp.qubo.dtos.questionboard.QuestionBoardDetailsDto;
 import nl.tudelft.oopp.qubo.sceneloader.SceneLoader;
 import org.testfx.framework.junit5.Start;
-
-import java.io.IOException;
 
 /**
  * This class tests the ModeratorViewController which controls the ModeratorView.fxml
@@ -15,18 +11,20 @@ import java.io.IOException;
 class ModeratorViewControllerTest extends TestFxBase {
 
     /*
-        These dto's are used in various tests as well as in @Start
+        Create an open Question Board which is loaded in @Start.
      */
-    QuestionBoardDetailsDto qcOpen = createOpenQuBo();
+    QuestionBoardDetailsDto qdOpen = createOpenQuBo();
 
     /**
      * Initiate testing done through the TestFX library.
      *
      * @param stage Test stage created by the TestFX library.
-     * @throws IOException IOException thrown by incorrect load in start method.
      */
     @Start
-    void start(Stage stage) throws IOException {
-        new SceneLoader().viewLoader(qcOpen, stage, "", "ModeratorView", getModCodeOpen());
+    void start(Stage stage) {
+        new SceneLoader().viewLoader(qdOpen, stage, "", "StudentView", qdOpen.getId());
     }
+
+
+    
 }
--- client/src/test/java/nl/tudelft/oopp/qubo/controllers/StudentViewControllerTest.java
+++ client/src/test/java/nl/tudelft/oopp/qubo/controllers/StudentViewControllerTest.java
@@ -15,54 +15,20 @@ import java.io.IOException;
 class StudentViewControllerTest extends TestFxBase {
 
     /*
-        These dto's are used in various tests as well as in @Start.
+        Create an open Question Board which is loaded in @Start.
      */
-    QuestionBoardDetailsDto qcOpen = createOpenQuBo();
-    QuestionBoardDetailsDto qcClosed = createClosedQuBo();
+    QuestionBoardDetailsDto qdOpen = createOpenQuBo();
 
     /**
      * Initiate testing done through the TestFX library.
      *
      * @param stage Test stage created by the TestFX library.
-     * @throws IOException Thrown by incorrect load in start method.
      */
     @Start
-    void start(Stage stage) throws IOException {
-        new SceneLoader().viewLoader(qcOpen, stage, "", "StudentView", qcOpen.getId());
+    void start(Stage stage) {
+        new SceneLoader().viewLoader(qdOpen, stage, "", "StudentView", qdOpen.getId());
     }
 
-    /**
-     * Show hide side screen.
-     */
-    @Test
-    void showHideSideScreen() {
-    }
 
-    /**
-     * Show hide ans questions.
-     */
-    @Test
-    void showHideAnsQuestions() {
-    }
 
-    /**
-     * Show ans questions.
-     */
-    @Test
-    void showAnsQuestions() {
-    }
-
-    /**
-     * Show hide polls.
-     */
-    @Test
-    void showHidePolls() {
-    }
-
-    /**
-     * Show polls.
-     */
-    @Test
-    void showPolls() {
-    }
 }
