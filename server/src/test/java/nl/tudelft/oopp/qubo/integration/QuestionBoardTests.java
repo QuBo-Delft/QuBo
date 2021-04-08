228f0e5d266b88a9bb98bc630a5e6424e0aad4d5
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-04-08 19:49:46 +0300

    Add tests for retrieveQuestionBoardDetails

--- server/src/test/java/nl/tudelft/oopp/qubo/integration/QuestionBoardTests.java
+++ server/src/test/java/nl/tudelft/oopp/qubo/integration/QuestionBoardTests.java
@@ -1,8 +1,10 @@
 package nl.tudelft.oopp.qubo.integration;
 
 import java.sql.Timestamp;
+import java.util.UUID;
 import nl.tudelft.oopp.qubo.dtos.questionboard.QuestionBoardCreationBindingModel;
 import nl.tudelft.oopp.qubo.dtos.questionboard.QuestionBoardCreationDto;
+import nl.tudelft.oopp.qubo.dtos.questionboard.QuestionBoardDetailsDto;
 import nl.tudelft.oopp.qubo.entities.QuestionBoard;
 import static nl.tudelft.oopp.qubo.integration.utils.JsonUtil.deserialize;
 import static nl.tudelft.oopp.qubo.integration.utils.JsonUtil.serialize;
@@ -27,6 +29,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -87,5 +90,62 @@ public class QuestionBoardTests {
         assertNotNull(inDb.getModeratorCode());
         assertNotEquals(inDb.getId(), inDb.getModeratorCode());
         assertFalse(inDb.isClosed());
+    }
+
+    @Test
+    public void retrieveQuestionBoardDetails_withValidBoardId_returnsCorrectBoard() throws Exception {
+        // Arrange
+        QuestionBoard qb = new QuestionBoard();
+        qb.setModeratorCode(UUID.randomUUID());
+        qb.setStartTime(Timestamp.valueOf("2021-03-01 00:00:00"));
+        qb.setTitle("Test board");
+        qb.setClosed(true);
+        questionBoardRepository.save(qb);
+
+        QuestionBoard qb2 = new QuestionBoard();
+        qb2.setModeratorCode(UUID.randomUUID());
+        qb2.setStartTime(Timestamp.valueOf("2021-03-02 00:00:00"));
+        qb2.setTitle("Test board 2");
+        qb2.setClosed(false);
+        questionBoardRepository.save(qb2);
+
+        // Act
+        ResultActions resultActions = mockMvc.perform(get("/api/board/{id}", qb.getId())
+            .contentType(MediaType.APPLICATION_JSON));
+
+        // Assert
+        MvcResult result = resultActions
+            .andExpect(status().isOk())
+            .andReturn();
+
+        QuestionBoardDetailsDto dto = deserialize(result.getResponse().getContentAsString(),
+            QuestionBoardDetailsDto.class);
+
+        assertEquals(qb.getId(), dto.getId());
+        assertEquals(qb.getTitle(), dto.getTitle());
+        assertEquals(qb.getStartTime(), dto.getStartTime());
+        assertEquals(qb.isClosed(), dto.isClosed());
+    }
+
+    @Test
+    public void retrieveQuestionBoardDetails_withNonexistentBoardId_returns404() throws Exception {
+        // Arrange
+        QuestionBoard qb = new QuestionBoard();
+        qb.setModeratorCode(UUID.randomUUID());
+        qb.setStartTime(Timestamp.valueOf("2021-03-01 00:00:00"));
+        qb.setTitle("Test board");
+        qb.setClosed(true);
+        questionBoardRepository.save(qb);
+
+        // Act
+        ResultActions resultActions = mockMvc.perform(get("/api/board/{id}", UUID.randomUUID())
+            .contentType(MediaType.APPLICATION_JSON));
+
+        // Assert
+        MvcResult result = resultActions
+            .andExpect(status().isNotFound())
+            .andReturn();
+
+        assertEquals("Unable to find resource", result.getResponse().getErrorMessage());
     }
 }
