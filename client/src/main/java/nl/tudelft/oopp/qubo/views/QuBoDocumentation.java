19c96172352933b81858f531d286fc87a3da3eef
Yunhan Wang <Y.Wang-128@student.tudelft.nl>
2021-04-09 10:21:38 +0200

    Rephrase documentation

--- client/src/main/java/nl/tudelft/oopp/qubo/views/QuBoDocumentation.java
+++ client/src/main/java/nl/tudelft/oopp/qubo/views/QuBoDocumentation.java
@@ -38,79 +38,80 @@ public class QuBoDocumentation {
 
         // Add the explanation HBoxes of icons to the layout
         layout.getChildren().add(getHBoxOf("status_open",
-                "It indicates that this question board is currently open."));
+                "Indicates that this question board is currently open."));
         layout.getChildren().add(getHBoxOf("status_closed",
-                "It indicates that this question board is currently closed."));
+                "Indicates that this question board is currently closed."));
         layout.getChildren().add(getHBoxOf("status_scheduled",
-                "It indicates that this question board will open as scheduled."));
+                "Indicates that this question board will open as scheduled."));
 
         if (!isStudentView) {
             window.setTitle("QuBo Documentation for Moderators");
             layout.getChildren().add(getHBoxOf("triangle_pace_bar",
-                    "It indicates the pace of this lecture as students experienced, "
+                    "Indicates the pace of this lecture as students experienced, "
                             + "the higher it locates the faster the lecture is experienced."));
         }
 
         if (isStudentView) {
             window.setTitle("QuBo Documentation for Students");
             layout.getChildren().add(getHBoxOf("btn_ask",
-                    "Click it to post a question on this question board."));
+                    "Click to post a question on this question board."));
         }
 
         layout.getChildren().add(getHBoxOf("upvote_true",
-                "Click it to upvote a question, click again to cancel the previous upvote."));
+                "Click to upvote a question, click again to cancel the previous upvote."));
         layout.getChildren().add(getHBoxOf("upvote_false",
-                "It indicates that this question has not been voted by the user."));
+                "Indicates that this question has not been voted by the user."));
 
         layout.getChildren().add(getHBoxOf("options",
-                "Click it to open the option menu for the question."));
-        layout.getChildren().add(getHBoxOf("delete", "Click it to delete this question."));
-        layout.getChildren().add(getHBoxOf("edit", "Click it to edit this question."));
+                "Click to open the option menu for the question."));
+        layout.getChildren().add(getHBoxOf("delete", "Click to delete this question."));
+        layout.getChildren().add(getHBoxOf("edit", "Click to edit this question."));
 
         if (!isStudentView) {
             layout.getChildren().add(getHBoxOf("ban",
-                    "Click it to ban the student who posted this question from the question board."));
+                    "Click to ban the student who posted this question from the question board."));
             layout.getChildren().add(getHBoxOf("mark_as_answered",
-                    "Click it to mark this question as answered."));
-            layout.getChildren().add(getHBoxOf("reply", "Click it to answer this question."));
+                    "Click to mark this question as answered."));
+            layout.getChildren().add(getHBoxOf("reply", "Click to answer this question."));
         }
 
         layout.getChildren().add(getHBoxOf("board_details",
-                "Click it to display the details of this question board."));
+                "Click to display the details of this question board."));
 
         if (isStudentView) {
             layout.getChildren().add(getHBoxOf("help",
-                    "Click it to display the guide of using this question board as a student."));
+                    "Click to display the guide of using this question board as a student."));
         }
 
         if (!isStudentView) {
             layout.getChildren().add(getHBoxOf("help",
-                    "Click it to display the guide of using this question board as a moderator."));
+                    "Click to display the guide of using this question board as a moderator."));
         }
 
         layout.getChildren().add(getHBoxOf("hamburger",
-                "Click it to open the additional option menu on the right side of the window."));
+                "Click to open the additional option menu on the right side of the window."));
         layout.getChildren().add(getHBoxOf("close_sidebar",
-                "Click it to hide the additional option menu."));
+                "Click to hide the additional option menu."));
 
         layout.getChildren().add(getHBoxOf("ans_q",
-                "Click it to open a section that displays all answered questions, "
+                "Click to open a section that displays all answered questions, "
                         + "click again to close this section."));
         layout.getChildren().add(getHBoxOf("polls",
-                "Click it to open a section that displays the details of a poll, "
+                "Click to open a section that displays the details of a poll, "
                         + "click again to close this section."));
 
         if (!isStudentView) {
             layout.getChildren().add(getHBoxOf("btn_export",
-                    "Click it to export a list of most important questions of this lecture to a text file."));
+                    "Click to export a list of all questions of this lecture and their answers to"
+                            + " a text file."));
         }
 
         layout.getChildren().add(getHBoxOf("leave",
-                "Click it to choose to leave this question board."));
+                "Click to choose to leave this question board."));
 
         if (!isStudentView) {
             layout.getChildren().add(getHBoxOf("btn_closeBoard",
-                    "Click it to close this question board."));
+                    "Click to close this question board."));
         }
 
         HBox bottomHBox = new HBox(20);
